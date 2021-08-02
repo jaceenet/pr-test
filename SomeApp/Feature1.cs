7a8bc1df5832fc7ba42bead7919a04681362c339
jaceenet <jacee.net@gmail.com>
2021-08-02 19:32:37 +0200

    feature 1

---
+++ SomeApp/Feature1.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Autofac;
+
+namespace SomeApp
+{
+    public class Feature1 : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            base.Load(builder);
+            builder.RegisterType<Dictionary<string, object>>().As<IDictionary<string, object>>();
+        }
+    }
+}
