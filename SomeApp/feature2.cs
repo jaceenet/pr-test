9e14ebf4f3f5de0aa41099b963ef909a04e24631
Jacob Carstensen <jacee.net@gmail.com>
2021-08-04 10:11:26 +0200

    added feature2 file (#4)
    line 3

---
+++ SomeApp/feature2.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace SomeApp
+{
+    public class Feature2
+    {
+        public static void Run()
+        {
+            var item = new Dictionary<string, object>();
+            item.Add("key1", 1);
+            item.Add("key2", 2);
+            item.Add("key3", 3);
+        }
+    }
+}
